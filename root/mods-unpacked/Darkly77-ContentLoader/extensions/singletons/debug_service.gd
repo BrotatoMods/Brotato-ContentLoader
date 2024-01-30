0c573f18743129844f524df6355130ecfb9f83e0
Kai <kai@kana.jetzt>
2024-01-30 09:25:12 +0100

    fix: :bug: fix for game update 1.0.1.3
    moved `ContentLoader._install_data()` call to `utils.gd` extension.
    
    Possible breaks mods that add their custom data not in their `mod_main` `_ready()` function.

--- root/mods-unpacked/Darkly77-ContentLoader/extensions/singletons/debug_service.gd
+++
@@ -1,9 +0,0 @@
-extends "res://singletons/debug_service.gd"
-
-# We're extending progress_data here because we need the game to finish loading
-# its data before we can add items
-
-func _ready():
-
-	var ContentLoader = get_node("/root/ModLoader/Darkly77-ContentLoader/ContentLoader")
-	ContentLoader._install_data()
